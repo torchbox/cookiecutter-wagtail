3dc3e713a69462b0d01df8c0006b1a80c682459d
Karl Hobley <karlhobley10@gmail.com>
2015-03-05 16:40:36 +0000

    Fixes to puppet config

--- {{cookiecutter.repo_name}}/puppet/{{cookiecutter.repo_name}}-production.pp
+++ {{cookiecutter.repo_name}}/puppet/{{cookiecutter.repo_name}}-production.pp
@@ -18,7 +18,8 @@ class wagtail::site::production::{{ cookiecutter.repo_name }}wagtail inherits wa
         staticdir        => "static",
         mediadir         => "media",
         deploy           => [ '@admin' ], # CHANGEME
-        python_version   => '3.4-local',
+        python_version   => '3.4',
+        pg_version       => '9.4',
         manage_daemons   => [
             'celery worker -C -c1 -A {{ cookiecutter.repo_name }}',
             'celery beat -A {{ cookiecutter.repo_name }} -C -s $TMPDIR/celerybeat.db --pidfile=',
--- {{cookiecutter.repo_name}}/puppet/{{cookiecutter.repo_name}}-staging.pp
+++ {{cookiecutter.repo_name}}/puppet/{{cookiecutter.repo_name}}-staging.pp
@@ -18,7 +18,8 @@ class wagtail::site::staging::{{ cookiecutter.repo_name }}wagtail inherits wagta
         staticdir        => "static",
         mediadir         => "media",
         deploy           => [ '@admin' ], # CHANGEME
-        python_version   => '3.4-local',
+        python_version   => '3.4',
+        pg_version       => '9.4',
         manade_daemons   => [
             'celery worker -C -c1 -A {{ cookiecutter.repo_name }}',
             'celery beat -A {{ cookiecutter.repo_name }} -C -s $TMPDIR/celerybeat.db --pidfile=',
