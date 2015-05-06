9bd88d618223b47a520bfd3e2f70ae1cfcc3b02e
Karl Hobley <karlhobley10@gmail.com>
2015-05-06 10:26:44 +0100

    Add views for testing 404 and 500 templates

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -18,13 +18,24 @@ urlpatterns = [
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^search/', include(wagtailsearch_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
-
-    url(r'', include(wagtail_urls)),
 ]
 
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.views.generic import TemplateView
 
+    # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # Add views for testing 404 and 500 templates
+    urlpatterns += [
+        url(r'^test404/$', TemplateView.as_view(template_name='404.html')),
+        url(r'^test500/$', TemplateView.as_view(template_name='500.html')),
+    ]
+
+
+urlpatterns += [
+    url(r'', include(wagtail_urls)),
+]
