5ad5dc23ad3ad26e16ee821087c5379fcb58fead
Karl Hobley <karlhobley10@gmail.com>
2015-11-16 10:33:37 +0000

    Add a couple more settings to dev.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/dev.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/dev.py
@@ -8,6 +8,9 @@ TEMPLATES[0]['OPTIONS']['debug'] = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'CHANGEME!!!'
 
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
+
+BASE_URL = 'http://localhost:8000'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
