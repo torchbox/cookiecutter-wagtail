d17703a54930a755422dd0d25c80f65150c74acb
Karl Hobley <karlhobley10@gmail.com>
2015-12-03 15:56:58 +0000

    Update django-redis

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
@@ -146,11 +146,11 @@ COMPRESS_PRECOMPILERS = (
 
 CACHES = {
     'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
+        'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': '127.0.0.1:6379',
         'KEY_PREFIX': '{{ cookiecutter.repo_name }}',
         'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }
