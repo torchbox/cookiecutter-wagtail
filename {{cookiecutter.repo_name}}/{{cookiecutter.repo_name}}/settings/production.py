5c906cad2d3116acc11e9ff7df55268fd98ac0b5
Karl Hobley <karlhobley10@gmail.com>
2015-12-21 10:36:03 +0000

    More verbose logging by default
    Added time, process/thread IDs and level to the log line.

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/production.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/production.py
@@ -144,16 +144,23 @@ LOGGING = {
             'class': 'django.utils.log.AdminEmailHandler',
         },
     },
+    'formatters': {
+        'default': {
+            'verbose': '[%(asctime)s] (%(process)d/%(thread)d) %(name)s %(levelname)s: %(message)s'
+        }
+    },
     'loggers': {
         'django.request': {
             'handlers':     ['mail_admins'],
             'level':        'ERROR',
             'propagate':    False,
+            'formatter':    'verbose',
         },
         'django.security': {
             'handlers':     ['mail_admins'],
             'level':        'ERROR',
             'propagate':    False,
+            'formatter':    'verbose',
         },
     },
 }
