cc6c0460787eafe92f232e7ccea8fc1e1f7b8e89
Karl Hobley <karl@torchbox.com>
2016-08-02 10:02:53 +0100

    A couple of tweaks to fab deploy tasks

--- {{cookiecutter.repo_name}}/fabfile.py
+++ {{cookiecutter.repo_name}}/fabfile.py
@@ -14,12 +14,11 @@ def deploy_production():
     # Remove this line when you're happy that this task is correct
     raise RuntimeError("Please check the fabfile before using it")
 
-    run('git pull origin master')
+    run('git pull')
     run('pip install -r requirements.txt')
     run('django-admin migrate --noinput')
     run('django-admin collectstatic --noinput')
     run('django-admin compress')
-    run('django-admin update_index')
 
     # 'restart' should be an alias to a script that restarts the web server
     run('restart')
@@ -30,12 +29,11 @@ def deploy_staging():
     # Remove this line when you're happy that this task is correct
     raise RuntimeError("Please check the fabfile before using it")
 
-    run('git pull origin staging')
+    run('git pull')
     run('pip install -r requirements.txt')
     run('django-admin migrate --noinput')
     run('django-admin collectstatic --noinput')
     run('django-admin compress')
-    run('django-admin update_index')
 
     # 'restart' should be an alias to a script that restarts the web server
     run('restart')
